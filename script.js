ac1b6af145a3e508193adef202820acb5584f59b
William Steinke de Mello <62147577+lion772@users.noreply.github.com>
2021-06-25 22:22:06 +0200

    Adding Js file

--- script.js
+++ script.js
@@ -1,143 +1,27 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>William's Personal Site</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <link rel="icon" href="images/favicon.ico" />
-  </head>
+window.onload = function () {
+  // add window.onload here and set it euqal to a function
+  // Get the modal
+  let modal = document.getElementById("myModal");
 
-  <body>
-    <header>
-      <div class="title-name">
-        <img id="cloud1" class="cloud" src="images/cloud1.png" alt="cloud" />
-        <img id="cloud2" class="cloud" src="images/cloud2.png" alt="cloud" />
-        <img id="cloud3" class="cloud" src="images/cloud2.png" alt="cloud" />
-        <img id="cloud4" class="cloud" src="images/cloud3.png" alt="cloud" />
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  let img = document.querySelector("#myImg");
+  let modalImg = document.getElementById("img01");
+  let captionText = document.getElementById("caption");
+  img.addEventListener("click", function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  });
 
-        <h1>I'm William</h1>
-        <h2>a programmer</h2>
-        <img id="mountain" src="images/mountain.png" alt="mountain" />
-      </div>
-    </header>
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
 
-    <div id="myModal" class="modal">
-      <span class="close">×</span>
-      <img src="" class="modal-content" id="img01" />
-      <div id="caption"></div>
-    </div>
+  // Get the <span> element that closes the modal
+  let span = document.querySelector(".close");
 
-    <div class="middle-container">
-      <div class="profile">
-        <img
-          id="myImg"
-          src="images/profile-picture.jpg"
-          alt="That's me in real life"
-        />
-        <h2>Hello!</h2>
-        <p>Former Android Developer and future Full-stack Web Developer ❤️</p>
-      </div>
-      <hr />
-
-      <div class="skills">
-        <h2>My Skills</h2>
-        <div>
-
-          <div class="skill-row">
-            <img class="images" src="images/mobile.png" alt="mobile" />
-
-            <h2>Android Development</h2>
-
-            <div class="paragraphs-format">
-              <p>
-              My first experience as a developer took place in São Paulo, as
-              Junior Android developer at NetPOS. There I learned a lot about the
-              newest technologies used in the labor market.
-              </p>
-            </div>
-          </div>
-          
-          <div class="skill-row">
-            <img class="images" src="images/computer1.png" alt="computer" />
-
-            <h2>Web Development</h2>
-
-            <div class="paragraphs-format">
-              <p>
-                I decided to start a new journey as Full-Stack developer since I
-                moved out to Germany, in order to expand my knowledge about this
-                area and go deeper about the real meaning of being a good developer.
-              </p>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-      <div class="clearfix"></div>
-
-      </div>
-
-          <div class="lottie">
-            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-            <lottie-player src="https://assets7.lottiefiles.com/private_files/lf30_sbsvs4ki.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>
-          </div>        
-          <h2>Goal</h2>
-
-          <div class="goals-format">
-            <div class="paragraphs-format">
-            <p>Since I moved to Germany, I've had decided to change my life completely, in order to improve some aspects of my life, like my German and my professional career.
-            <p>You can be part of it by just giving me a good recommendation to your friends, superiors or even your boss! It'd be very kind if you could tell them that there's someone who's really worth contracting.</p> 
-            </p>
-          </div>
-         </div>
-        </div>
-        <hr />
-        <img class="" src="images/chillies.png" alt="Purple cat programming" />
-        <h2>A little more about me</h2>
-
-        <div class="goals-format">
-
-          <div class="paragraphs-format">
-            <p>
-              It's hard to enumerate all my best skills, but besides my knowledge
-              in 4 foreign languages and acquaintance in Java, Kotlin, Python and
-              now Javascript.
-            </p>
-            <p>I'd say that my best characteristic is being always
-              very hard-working learner and passionate about programming has
-              become one of my favorite hobbies, which makes up 70% of my day.
-            </p>
-          </div>
-
-          
-        </div>
-      
-      <hr />
-
-      <h2>Get In Touch</h2>
-
-      <div class="contact-me">
-        <h3>If you have any questions, do not hesitate to ask.</h3>
-        <a class="link-email button" href="mailto:name@email.com">Write me an email</a>
-        <h3>Or talk to me through my social network</h3>
-      </div>
-    </div>
-
-    <div class="bottom-container-content">
-      <div class="bottom-container">
-        <a href="https://www.linkedin.com/in/william-steinke-de-mello/"
-          >LinkedIn</a
-        >
-        <span style="color: #fff;" href="#">&mdash;</span>
-        <a href="https://www.facebook.com/will.lopes.104">Facebook</a>
-        <p>© 2021 William Steinke de Mello.</p>
-      </div>
-    </div>
-
-    <script type="text/javascript" src="script.js"></script>
-  
-  
-
-  </body>
-</html>
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+}; // close the function
